23df589023777cd7b15007bd9ca9a06b20a5c3d9
Thabiso Pheko <tabs150@gmail.com>
2025-02-04 09:02:17 +0200

    ci: initiate ecom project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,13 @@ export default function Home() {
           className="dark:invert"
           src="/next.svg"
           alt="Next.js logo"
-          width={180}
+          width={180} 
           height={38}
           priority
         />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing{" "}
+            Get started by editing{" "} Hello World {" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
               src/app/page.tsx
             </code>
